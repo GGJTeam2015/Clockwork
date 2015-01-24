a01dfaa3dc940c250e937fb10c1b726439b281e3
KemalTy <y.kemal.tugrul@gmail.com>
2015-01-24 17:18:17 +0200

    Spacecraft Item Control
    v. 1.0

---
+++ Assets/Clockwork Assets/Scripts/SpacecraftItemControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpacecraftItemControl : MonoBehaviour {
+
+    // Singleton Instance
+    private static SpacecraftItemControl inst = null;
+
+    // Object prefabs
+    public List<GameObject> itemPrefabs = new List<GameObject>(5);
+    public float prefabDropProb = 0.1f;
+
+    // Getter
+    public static SpacecraftItemControl Inst { get { return inst; } }
+
+    void Awake()
+    {
+        if (inst == null)
+        { inst = this; }
+    }
+
+	// Get a prefab
+    public GameObject giveMeAnItem()
+    {
+        // Drop?
+        if (Random.Range(0.0f, 1.0f) < prefabDropProb)
+        {
+            // Drop which?
+            int idx = Random.Range(0, itemPrefabs.Count);
+            return itemPrefabs[idx];
+        }
+
+        else
+        {
+            return null;
+        }
+    }
+
+}
