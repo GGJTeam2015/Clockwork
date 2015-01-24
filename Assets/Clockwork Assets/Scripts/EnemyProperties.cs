dd92d45dd15ad9c245939f6d541b7bfec7fde5fb
KemalTy <y.kemal.tugrul@gmail.com>
2015-01-24 15:56:49 +0200

    Enemy Prefab Minor Change

---
+++ Assets/Clockwork Assets/Scripts/EnemyProperties.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyProperties : Properties {
+
+    public override void Damage(int damageAmount)
+    {
+        base.Damage(damageAmount);
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("An enemy died");
+
+        Destroy(this.gameObject);
+    }
+}
