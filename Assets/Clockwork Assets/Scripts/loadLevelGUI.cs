225c00aa24a90e3e425a45b476f891b66ca6a8fc
KemalTy <y.kemal.tugrul@gmail.com>
2015-01-25 13:44:50 +0200

    Levels

---
+++ Assets/Clockwork Assets/Scripts/loadLevelGUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadLevelGUI : MonoBehaviour {
+
+	// Use this for initialization
+	public void newGame()
+	{
+		Application.LoadLevel("Level1");
+	}
+
+	public void mainMenu()
+	{
+		Application.LoadLevel("MainMenu");
+	}
+
+	public void credits()
+	{
+		Application.LoadLevel("Credits");
+	}
+
+	public void exit()
+	{
+		Application.Quit();
+	}
+}
